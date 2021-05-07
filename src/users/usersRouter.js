8bf06cfc3854c6b53bf0f22ab43161ecbd4b038d
Greg Johle <greg.johle@gmail.com>
2021-05-07 15:27:28 -0400

    added route to add a user

--- src/users/usersRouter.js
+++ src/users/usersRouter.js
@@ -1,94 +1,34 @@
-const path = require("path"),
-  express = require("express"),
+const express = require("express"),
+  path = require("path"),
   xss = require("xss"),
-  uuid = require("uuid"), //Use in /POST to create uuid for id.
-  UsersService = require("./folders-service"),
-  usersRouter = express.Router(),
-  jsonParser = express.json();
+  UsersService = require("./usersService.js"),
+  UsersRouter = express.Router(),
+  jsonParser = express.json(),
+  bcrypt = require("bcryptjs");
 
-const serializeUser = (user) => ({
-  id: user.id,
-  name: xss(user.name),
-});
-
-usersRouter
-  .route("/")
+UsersRouter.route("/")
   .get((req, res, next) => {
-    const knexInstance = req.app.get("db");
-    UsersService.getAllUsers(knexInstance)
-      .then((users) => {
-        res.json(users.map(serializeUser));
-      })
+    UsersService.getByEmail(req.app.get("db"), req.body.email)
+      .then((user) => {})
       .catch(next);
   })
-  .post(jsonParser, (req, res, next) => {
-    const { name, email, password } = req.body,
-      newUser = { name, email, password };
-
-    for (const [key, value] of Object.entries(newUser)) {
-      if (value == null) {
-        return res.status(400).json({
-          error: { message: `Missing ${key} in request body` },
-        });
-      }
-    }
-    newUser.id = uuid();
-    newUSer.name = xss(newUser.name);
-
-    FoldersService.insertFolder(req.app.get("db"), newFolder)
-      .then((folder) => {
-        res
-          .status(201)
-          .location(path.posix.join(req.originalUrl, `/${folder.id}`))
-          .json(serializeFolder(folder));
-      })
-      .catch(next);
-  });
-
-foldersRouter
-  .route("/:folder_id")
-  .all((req, res, next) => {
-    FoldersService.getById(req.app.get("db"), req.params.folder_id)
-      .then((folder) => {
-        if (!folder) {
-          return res.status(404).json({
-            error: { message: "No matching folder" },
-          });
+  .post((req, res, next) => {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(req.body.password, process.env.SALT);
+    const newUser = { name, email, hashedPassword };
+    UsersService.getByEmail(req.app.get("db"), req.body.email)
+      .then((user) =>{
+        if (user) {
+          res.send("A User already exists with that email.")
         }
-        res.folder = folder;
-        next();
+        if (!user) {
+          UsersService.insertUser(req.app.get("db"), newUser)
+          .then((res) => {
+            res.send("user added")
+          })
+          .catch(next)
+        }
       })
-      .catch(next);
-  })
-  .get((req, res, next) => {
-    res.json(serializeFolder(res.folder));
-  })
-  .delete((req, res, next) => {
-    FoldersService.deleteFolder(req.app.get("db"), req.params.folder_id)
-      .then((numRowsAffected) => {
-        res.status(204).end();
-      })
-      .catch(next);
-  })
-  .patch(jsonParser, (req, res, next) => {
-    const { name } = req.body,
-      folderToUpdate = { name };
 
-    const numberOfValues = Object.values(folderToUpdate).filter(Boolean).length;
-    if (numberOfValues === 0)
-      return res.status(400).json({
-        error: { message: "Body must contain name." },
-      });
-
-    FoldersService.updateFolder(
-      req.app.get("db"),
-      req.params.folder_id,
-      folderToUpdate
-    )
-      .then((numRowsAffected) => {
-        res.status(204).end();
-      })
-      .catch(next);
+    UsersService.insertUser(req.app.get("db"), newUser)
   });
-
-module.exports = foldersRouter;
