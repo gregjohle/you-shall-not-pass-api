af2a36c92b09ebe2c908250644f2c9670addaaae
Greg Johle <greg.johle@gmail.com>
2021-05-12 14:35:07 -0400

    added functionality to insert new passwords

--- src/passwords/passwordsRouter.js
+++ src/passwords/passwordsRouter.js
@@ -5,14 +5,36 @@ const xss = require("xss");
 const PasswordsService = require("./passwordsService");
 const PasswordsRouter = express.Router();
 const jsonParser = express.json();
-const cryptr = require("cryptr");
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr(process.env.SECRET);
+const passport = require("passport");
 
-PasswordsRouter.route("/passwords").get((req, res) => {
-  const { id } = req.session;
-  console.log(req.session);
-  // PasswordsService.getAllPasswords(req.app.get("db"), id).then((passwords) => {
-  //   return res.json({ passwords });
-  // });
-});
+PasswordsRouter.route("/passwords")
+  .get((req, res) => {
+    const user = passport.deserializeUser(req.sessionID, done);
+
+    PasswordsService.getAllPasswords(req.app.get("db"), user.id).then(
+      (passwords) => {
+        return res.json({ passwords });
+      }
+    );
+  })
+  .post((req, res) => {
+    const name = xss(req.body.name);
+    const site = xss(req.body.site);
+    const password = crpytr.encrypt(req.body.password);
+    const userId = "1";
+
+    const newPassword = {
+      name: name,
+      site: site,
+      password: password,
+      user_id: userId,
+    };
+    console.log(newPassword);
+    PasswordsService.insertPassword(req.app.get("db"), newPassword).then(
+      res.status(200).send("password added")
+    );
+  });
 
 module.exports = PasswordsRouter;
