a7f5a8f6df5c0ee620eff3335a44d916df428ed7
Greg Johle <greg.johle@gmail.com>
2021-05-20 16:07:39 -0400

    troubleshooting heroku

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { PORT } = require("./config");
 const db = knex({
   client: "pg",
   connection: {
-    connection: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     // ssl: true,
     ssl: { rejectUnauthorized: false },
   },
